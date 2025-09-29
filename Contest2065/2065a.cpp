1d59e7dbb9235b92bae8a975c1304863448ddffd
Gobson995 <gugabadazanatta@gmail.com>
2025-09-28 21:00:15 -0300

    a da div 4

---
+++ Contest2065/2065a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int tc; cin >> tc;
+
+  while (tc--) {
+    string s; cin >> s;
+
+    for (int i = 0; i <(s.size() - 2); i++) {
+      cout << s[i];
+    }
+
+    cout << "i" << endl;
+  }
+}
