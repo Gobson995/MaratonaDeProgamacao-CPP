8996acaa29cf91fec15416f7bf237a96532c35e6
Gobson995 <gugabadazanatta@gmail.com>
2025-10-18 10:32:18 -0300

    tentativa de 2162c

--- SemResposta/2162c.cpp
+++ SemResposta/2162c.cpp
@@ -1,30 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+#define int long long
 
-signed main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+signed main() {
+  int t;
+  cin >> t;
 
-  int tc;
-  cin >> tc;
-
-  while (tc--) {
-    long long a, b;
+  while (t--) {
+    int a, b;
     cin >> a >> b;
 
+    int x = a ^ b;
+
+
     if (a == b) {
-        cout << 0 << endl;
-    } 
-    else if ((a ^ b) <= a) {
-        cout << 1 << endl << (a ^ b) << endl;
-    } 
-    else if (true) {
-        long long x1 = (a ^ b) & ((1LL << 30) - 1);
-        long long a1 = a ^ x1;
-        long long x2 = a1 ^ b;
-        cout << 2 << endl << x1 << " " << x2 << endl;
+      cout << 0 << endl;
     }
+    else if (x <= a) {
+      cout << 1 << endl << x << endl;
+    }
+    else if (a > b) {
+      int mid = (a ^ b) / 2;
+      int x1 = a ^ mid;
+      int x2 = mid;
+
+      cout << 2 << endl << x1 << " " << x2 << endl;
+    }
+    else {
+      cout << -1 << endl;
+    }
+
+    
   }
 }
