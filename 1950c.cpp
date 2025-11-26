b43377f2bf60ff85b7cb39b4f94ffc772758799b
Gobson995 <gugabadazanatta@gmail.com>
2025-11-26 18:32:01 -0300

    questão quase feita

---
+++ 1950c.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int tc; cin >> tc;
+
+  while (tc--) {
+    string a; cin >> a;
+
+    int p = a[0] - '0';
+    int s = a[1] - '0';
+
+    int m1 = a[3];
+    int m2 = a[4];
+
+    int h = 0;
+    h += (10*p) + s;
+
+
+    string opa;
+
+    if (h < 12) opa = "AM";
+    else opa = "PM";
+
+    if (h == 0) h = 12;
+    else if (h > 12) h -= 12;
+
+    if (h < 10) cout << "0";
+    
+    cout << h << ":" << m1 << m2 << " " << opa << endl;
+  }
+}
