0c5e89e103e809b616fe9cc880478ca1fd4dc25c
Gobson995 <gugabadazanatta@gmail.com>
2025-12-02 21:52:03 -0300

    questao do dia feito

---
+++ Contest2126div3/2126a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int tc; cin >> tc;
+
+  while(tc--) {
+    string a; cin >> a;
+    sort(a.begin(),a.end());
+
+    cout << a[0] << endl;
+  }
+}
