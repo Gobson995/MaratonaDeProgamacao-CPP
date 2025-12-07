6625ba333fd60ba4d7bd27d733f178c9c9df659f
Gobson995 <gugabadazanatta@gmail.com>
2025-12-06 23:08:39 -0300

    tentando fazer questão

---
+++ SemResposta/2117a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int tc; cin >> tc;
+
+  while (tc--) {
+    int n, x;
+    cin >> n >> x;
+    vector<int>a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    bool opa = false;
+
+    for (int i = 0; i < n; i++) {
+      if (a[i] == 1) {
+        int esq = n - i;
+        if (esq <= x) opa = true;
+      }
+    }
+
+    if (opa) cout << "YES" << endl;
+    else cout << "NO" << endl;
+  }
+}
