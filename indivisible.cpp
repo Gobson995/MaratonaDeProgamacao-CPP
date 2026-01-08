c2c6ee1702efb8e4ec6fac9f38bbee1d08b016b6
Gobson995 <gugabadazanatta@gmail.com>
2026-01-07 21:45:40 -0300

    questão do dia feita

---
+++ indivisible.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int tc; cin >> tc;
+
+  while(tc--) {
+    int n; cin >> n;
+
+    if (n == 1) cout << 1 << endl;
+    else if (n % 2 == 1) cout << -1 << endl;
+    else {
+      for (int i = 1; i <= n; i+=2) {
+        cout << i + 1 << " " << i << " ";
+      }
+      cout << endl;
+    }
+  }
+}
