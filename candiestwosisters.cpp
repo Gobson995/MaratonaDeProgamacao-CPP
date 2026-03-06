ff327fbe8cf97c22024f2d1525634f3793dc99cd
Gobson995 <gugabadazanatta@gmail.com>
2026-03-06 20:43:36 -0300

    questão do dia feita

---
+++ candiestwosisters.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+signed main () {
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        cout << (n - 1) / 2 << endl;
+    }
+}
